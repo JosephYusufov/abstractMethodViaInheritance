1bd11ed61ac3edac6268627340f2ef816e255462
JosephYusufov <jyusufov00@stuy.edu>
2019-03-02 21:50:06 -0500

    Finished my abstractMethodViaInheritance project, now ready to collaborate with a classmate

--- UserOfSpeakers.java
+++ UserOfSpeakers.java
@@ -1,7 +1,7 @@
 public class UserOfSpeakers{
     public static void main(String[] commandLine){
 	Animal pup = new Retriever();
-	System.out.println(pup.speak);
+	System.out.println(pup.speak());
 
 	//error message when trying to instantiate an abstract class
 	System.out.println("UserOfSpeakers.java:5: error: Animal is abstract; cannot be instantiated"); 
