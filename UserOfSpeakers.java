41ed806dc02dd1b387f9f4685c890c0c9286edff
Leonard Melnik <leonard@onedr.io>
2019-03-05 13:10:26 -0500

    Removed intentional error

--- UserOfSpeakers.java
+++ UserOfSpeakers.java
@@ -3,8 +3,4 @@ public class UserOfSpeakers{
 	Animal pup = new Retriever();
 	System.out.println(pup.speak());
 
-	//error message when trying to instantiate an abstract class
-	System.out.println("UserOfSpeakers.java:5: error: Animal is abstract; cannot be instantiated"); 
-    }
 }
-
